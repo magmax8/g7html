0788ad9effb9d548e551a544ad606604857ea630
magmax8 <magmax8@hotmail.com>
2024-11-07 18:15:06 -0500

    Formulario con Validaciones ok

--- JS/distMenu.js
+++ JS/distMenu.js
@@ -1,15 +1,23 @@
-function fmenu(page){
+function fmenu(page) {
     fetch(`${page}.html`)
-        .then(response=>response.text())
-        .then(data=>{
-                    document.getElementById('cuerpoPag').innerHTML = data;
-              
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('cuerpoPag').innerHTML = data;
+                       
+                // Configurar Parsley validaciones de formulario
+                $('#registroForm').parsley({
+                    trigger: 'input'
+                });
+                // Para asegurar la validación inmediata mientras el usuario escribe
+                $('input').on('input', function () {
+                    $(this).parsley().validate();
+                });
+
                 // Seleccionar el modal llamandolo por su Id
                 var modal = new bootstrap.Modal(document.getElementById('exampleModal'));
                 modal.show(); // Muestra el modal
-              
-              
-                }).catch(error=> console.log('Error al cargar la página', error));
- 
+
+        }).catch(error => console.log('Error al cargar la página', error));
+
 }
 
